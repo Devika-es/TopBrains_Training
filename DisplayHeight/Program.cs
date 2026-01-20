7eb2980c9ec95a731843738eb0e76acd15a0728b
Devika E S <devikaes17@gmail.com>
2026-01-20 15:14:39 +0530

    Qn 7 Display Height

---
+++ DisplayHeight/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace DisplayHeight;
+class Program
+{
+    public static void Main()
+    {
+        System.Console.Write("Enter height: ");
+        int heightCm=Int32.Parse(Console.ReadLine());
+
+        string category="";
+        if(heightCm>=0 && heightCm<150)
+        {
+            category="Short";
+        }
+        else if(heightCm>=150 && heightCm<180)
+        {
+            category="Average";
+        }
+        else if(heightCm>=180 && heightCm<=300)
+        {
+            category="Tall";
+        }
+        else
+        {
+            category="Invalid";
+        }
+        System.Console.WriteLine("Category: "+category);
+    }
+}
