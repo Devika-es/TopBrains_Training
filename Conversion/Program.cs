cd45e105ed0915314e3aa74ff723041485df5a31
Devika E S <devikaes17@gmail.com>
2026-01-20 15:49:55 +0530

    Qn 19 Conversion Feet to Centimeters

---
+++ Conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Write a method that converts feet to centimeters.
+// Use: 1 foot = 30.48 cm
+// Round the result to 2 decimal places (MidpointRounding.AwayFromZero).
+
+// Input: feet (int)
+// Output: centimeters (double)
+
+
+using System;
+namespace Conversion;
+class Program
+{
+    public static void Main()
+    {
+        System.Console.Write("Enter feet: ");
+        int feet=Int32.Parse(Console.ReadLine());
+
+        double centimeters=feet*30.48;
+        System.Console.WriteLine($"Centimeters: {Math.Round(centimeters,2,MidpointRounding.AwayFromZero):F2}");
+    }
+}
