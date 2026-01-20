6aa30c79a74eabaeaaf407fa8cc5352babbe587f
Devika E S <devikaes17@gmail.com>
2026-01-20 15:04:51 +0530

    Qn3 Multiplication table

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Linq;
+namespace MultiplicationTable;
+class Program
+{
+    public static void Main()
+    {
+        System.Console.Write("Enter n: ");
+        int n=Int32.Parse(Console.ReadLine());
+
+        System.Console.Write("Enter upto: ");
+        int upto=Int32.Parse(Console.ReadLine());
+
+        int[] row =new int[upto];
+        for(int i=1;i<=upto;i++)
+        {
+            row[i-1]=n*i;
+        }
+        System.Console.WriteLine("["+string.Join(",",row)+"]");
+    }
+}
