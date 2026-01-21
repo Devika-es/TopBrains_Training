aee76a45e5bf106e467f40c15c99f28056f5ea18
Devika E S <devikaes17@gmail.com>
2026-01-21 15:08:50 +0530

    Qn 21 Time Conversion

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TimeConversion;
+class Program
+{
+    public static void Main()
+    {
+        System.Console.Write("Enter Total Seconds: ");
+        int totalSeconds=Int32.Parse(Console.ReadLine());
+
+        string formatted="";
+
+        int min=totalSeconds/60;
+        int sec=totalSeconds%60;
+        formatted=min+":"+sec.ToString("D2");
+
+        System.Console.WriteLine($"Formatted String: {formatted}");
+    }
+}
